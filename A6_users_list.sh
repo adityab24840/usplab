b2cc4489608a7f2086caf1938c85c0428cd7c870
Aditya N Bhatt <56029648+adityab24840@users.noreply.github.com>
2019-12-19 21:10:13 +0530

    Create A6_users_list.sh

---
+++ A6_users_list.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo “User logged in:”
+users
+echo “Current logged in date and time :” 
+date
+echo “Currently logged in users:” 
+who
+echo “Currently logged in username:” 
+whoami
+
+/*output:
+ [LabExam@ISELAB1 ~]$ sh pr6.sh
+
+[LabExam@ISELAB1 ~]$chmod a+x pr6.sh
+[LabExam@ISELAB1 ~]$sh pr6.sh
+
+
+User logged in:
+UNIX LAB UNIX LAB
+*/
